2728c6273a9b156a5ed72172fc467586f07717ab
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-09-13 17:52:03 +0900

    disables random goal and adds pose goal

--- src/move_group_test.cpp
+++ src/move_group_test.cpp
@@ -1,4 +1,5 @@
 #include <moveit/move_group_interface/move_group.h>
+#include <geometry_msgs/Pose.h>
 
 int main(int argc, char **argv)
 {
@@ -7,16 +8,41 @@ int main(int argc, char **argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  moveit::planning_interface::MoveGroup::Options options("arm", "/robot_description");
+  moveit::planning_interface::MoveGroup::Options options("arm", "robot_description");
   ROS_INFO_STREAM("Configuring move group interface ...");
   // this connecs to a running instance of the move_group node
   moveit::planning_interface::MoveGroup group(options);
   ROS_INFO_STREAM("Move group interface configured.");
+
   // specify that our target will be a random one
-  ROS_INFO_STREAM("Setting random target ...");
-  group.setRandomTarget();
-  // plan the motion and then move the group to the sampled target
-  ROS_INFO_STREAM("Moving arm ...");
+//  ROS_INFO_STREAM("Setting random target ...");
+//  group.setRandomTarget();
+//  // plan the motion and then move the group to the sampled target
+//  ROS_INFO_STREAM("Moving arm ...");
+//  if (group.move())
+//  {
+//    ROS_INFO_STREAM("Moving done.");
+//  }
+//  else
+//  {
+//    ROS_WARN_STREAM("Moving failed.");
+//  }
+
+  // specifying a goal pose
+  ROS_INFO_STREAM("Setting pose goal for end effector '" << group.getEndEffectorLink() << "'.");
+  geometry_msgs::Pose goal_pose;
+  goal_pose.position.x = 0.5;
+  goal_pose.position.y = 0.0;
+  goal_pose.position.z = 0.5;
+  goal_pose.orientation.x = 0.70711;
+  goal_pose.orientation.y = 0.0;
+  goal_pose.orientation.z = 0.0;
+  goal_pose.orientation.w = 0.70711;
+  ROS_INFO_STREAM("Pose goal:");
+  ROS_INFO_STREAM(goal_pose);
+
+  group.setPoseTarget(goal_pose);
+  group.setGoalTolerance(1e-5);
   if (group.move())
   {
     ROS_INFO_STREAM("Moving done.");
@@ -26,5 +52,6 @@ int main(int argc, char **argv)
     ROS_WARN_STREAM("Moving failed.");
   }
 
-  ros::waitForShutdown();
+  spinner.stop();
+//  ros::waitForShutdown();
 }
